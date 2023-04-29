d09968731f1e03bfd23a4c6a5ea469329d8ecc16
sivan marom <sivmamrom@gmail.com>
2023-04-29 18:24:14 +0300

    commit

--- app.py
+++ app.py
@@ -70,6 +70,8 @@ def launch_instance():
         if add_jenkins:
             user_data += 'docker run --name jenkins_master -p 8080:8080 -p 50000:50000 -d -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts\n'
             user_data += 'docker exec -u root jenkins_master bash -c "apt-get update && apt-get -y install docker.io"\n'
+            user_data += 'sudo chmod 666 /var/run/docker.sock'
+            user_data += 'sudo apt install python3'
         instance_name = request.form.get('instance_name')
         instance_type = request.form.get('instance_type')
         key_pair_name = 'jenkins-master'
