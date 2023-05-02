c77126ff6f6fbb441ea5ec05b4e3cb1ca2cf4043
sivan marom <sivmamrom@gmail.com>
2023-05-02 14:12:31 +0300

    created test

--- conftest.py
+++ conftest.py
@@ -3,10 +3,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from webdriver_manager.utils import ChromeType
 
 @pytest.fixture(scope="class")
 def setup():
-    chrome_driver_path = ChromeDriverManager().install()
+    chrome_driver_path = ChromeDriverManager(chrome_type=ChromeType.GOOGLE, log_level=0, path="/usr/bin/chromedriver").install()
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
     service_obj = Service(chrome_driver_path)
