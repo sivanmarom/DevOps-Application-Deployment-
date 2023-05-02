3e1f42f704a0043744d33d5b4dd0bfeccf311b7b
sivan marom <sivmamrom@gmail.com>
2023-05-02 13:38:48 +0300

    created test

--- conftest.py
+++ conftest.py
@@ -3,7 +3,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 @pytest.fixture(scope="class")
 def setup():
@@ -13,4 +12,4 @@ def setup():
     service_obj = Service(chrome_driver_path)
     driver = webdriver.Chrome(service=service_obj, options=chrome_options)
     driver.implicitly_wait(5)
-    yield driver
+    return driver
