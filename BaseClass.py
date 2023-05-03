c9d1522b1260ada81843de9b75acac734e1c3cac
sivan marom <sivmamrom@gmail.com>
2023-05-03 16:14:27 +0300

    new try

--- BaseClass.py
+++ BaseClass.py
@@ -1,10 +1,14 @@
 import logging
+import os
 class BaseClass:
     def log_conf(self):
         logger = logging.getLogger(__name__)
         fileHandler = logging.FileHandler("logfile.log")
-        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(message)s")
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(message)s : %(user)s:")
+        formatter.user = os.environ['BUILD_USER']
         fileHandler.setFormatter(formatter)
         logger.addHandler(fileHandler)
         logger.setLevel(logging.DEBUG)
         return logger
+
+
