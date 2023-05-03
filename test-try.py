ff2ae90db3ce93a8a712f9920cae26fdbf876da7
sivan marom <sivmamrom@gmail.com>
2023-05-03 14:03:17 +0300

    new try

--- test-try.py
+++ test-try.py
@@ -9,7 +9,7 @@ class Test_class(BaseClass):
         global driver
         log = self.log_conf()
         driver = setup
-        driver.get("http://54.234.117.205:5000/")
+        driver.get("http://3.85.123.55:5000/")
         sign_up = driver.find_element(By.CSS_SELECTOR, ".register_link")
         sign_up.click()
         name = 'sivan'
