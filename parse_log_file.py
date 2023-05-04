be2e3e96edd9ed34ba33f0fcc5e195e0a302135c
sivan marom <sivmamrom@gmail.com>
2023-05-04 15:21:05 +0300

    new try

--- parse_log_file.py
+++ parse_log_file.py
@@ -2,6 +2,6 @@ with open("logfile.log", "r") as f:
     last_line = f.readlines()[-1]
     entry = last_line.strip().split(" :")
     timestamp = entry[0]
-    message = entry[2]
+    message = entry[4]
 
 print(f"{timestamp},{message}")
