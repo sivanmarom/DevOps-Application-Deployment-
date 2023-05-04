78e81c67949adb814746419c9ec7fd3cc2f09124
sivan marom <sivmamrom@gmail.com>
2023-05-04 12:54:01 +0300

    new try

--- parse_log_file.py
+++ parse_log_file.py
@@ -1,10 +1,11 @@
-def parse_log_file(self):
-    with open(self.log_file_path, "r") as f:
-        last_line = f.readlines()[-1]
-        entry = last_line.strip().split(" :")
-        log_entry = {
-            "timestamp": entry[0],
-            "level": entry[1],
-            "message": entry[2]
-        }
-    return log_entry
+import json
+with open("logfile.log", "r") as f:
+    last_line = f.readlines()[-1]
+    entry = last_line.strip().split(" :")
+    log_entry = {
+        "timestamp": entry[0],
+        "level": entry[1],
+        "message": entry[2]
+    }
+
+print(json.dumps(log_entry))
