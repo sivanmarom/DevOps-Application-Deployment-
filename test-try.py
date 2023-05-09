fad9d7fa7f459f4908e5d1d4c0fef573b58ac19f
sivan marom <sivmamrom@gmail.com>
2023-05-09 08:46:26 +0300

    updated public ips

--- test-try.py
+++ test-try.py
@@ -9,7 +9,7 @@ class Test_class(BaseClass):
         global driver
         log = self.log_conf()
         driver = setup
-        driver.get("http://18.215.143.106:5000/")
+        driver.get("http://54.242.100.80:5000/")
         sign_up = driver.find_element(By.CSS_SELECTOR, ".register_link")
         sign_up.click()
         name = 'sivan'
